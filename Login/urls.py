9ad4022d611ccf9ab2d20f213792819c8404beb3
roya49 <52095763+roya49@users.noreply.github.com>
2024-02-08 15:39:53 +0800

    Update urls.py

--- Login/urls.py
+++ Login/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path("", views.Login, name="index"),
+     path("", views.login, name="index"),
      path('', views.index),
      path('', views.test),
 ]

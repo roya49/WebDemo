5d56a92d4b40791362af223b3c765d03ae4ce3c4
roya49 <52095763+roya49@users.noreply.github.com>
2024-02-08 15:38:16 +0800

    Add files via upload

--- Login/urls.py
+++ Login/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from Login import views
+from . import views
 
 urlpatterns = [
-     path("", views.Login.as_view(), name="index")
-
+     path("", views.Login, name="index"),
+     path('', views.index),
+     path('', views.test),
 ]
